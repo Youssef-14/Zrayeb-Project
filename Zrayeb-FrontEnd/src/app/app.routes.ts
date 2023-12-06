0ddba2c3c84a0a2bc4f840f66d1d6f35a7c16d10
Youssef-14 <youssefmaatoug99@gmail.com>
2023-12-06 15:43:22 +0100

    feat : Tour 16 : Page-Login-Frontend

--- Zrayeb-FrontEnd/src/app/app.routes.ts
+++ Zrayeb-FrontEnd/src/app/app.routes.ts
@@ -1,6 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomepageComponent} from "./pages/homepage/homepage.component";
 import {LoginpageComponent} from "./pages/loginpage/loginpage.component";
+import {RegisterpageComponent} from "./pages/registerpage/registerpage.component";
 
 export const routes: Routes = [
   {
@@ -14,5 +15,9 @@ export const routes: Routes = [
   {
     path: 'login',
     component: LoginpageComponent,
+  },
+  {
+    path: 'register',
+    component: RegisterpageComponent,
   }
 ];
