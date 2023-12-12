4ed8ceca57d5b673b184c6dd56ea2a9fe0f5f004
Bouzomita Naim <damdoumbouz@gmail.com>
2023-12-12 21:33:16 +0100

    homepageok

--- Zrayeb-FrontEnd/src/app/app.routes.ts
+++ Zrayeb-FrontEnd/src/app/app.routes.ts
@@ -1,8 +1,7 @@
 import { Routes } from '@angular/router';
-import {HomepageComponent} from "./pages/homepage/homepage.component";
-import {LoginpageComponent} from "./pages/loginpage/loginpage.component";
-import {RegisterpageComponent} from "./pages/registerpage/registerpage.component";
-
+import { HomepageComponent } from "./pages/homepage/homepage.component";
+import { LoginpageComponent } from "./pages/loginpage/loginpage.component";
+import { RegisterpageComponent } from "./pages/registerpage/registerpage.component";
 export const routes: Routes = [
   {
     path: '',
--- Zrayeb-FrontEnd/src/app/pages/homepage/homepage.component.ts
+++ Zrayeb-FrontEnd/src/app/pages/homepage/homepage.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import {NavbarComponent} from "../../components/navbar/navbar.component";
-import {FooterComponent} from "../../components/footer/footer.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
+import { FooterComponent } from "../../components/footer/footer.component";
+import { CardComponent } from "../../components/card/card.component";
 
 @Component({
   selector: 'app-homepage',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, FooterComponent],
+  imports: [CommonModule, NavbarComponent, FooterComponent, CardComponent],
   templateUrl: './homepage.component.html',
   styleUrl: './homepage.component.css'
 })
