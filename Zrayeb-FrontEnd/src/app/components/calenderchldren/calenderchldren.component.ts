9096e23e10c300e53136ff434acb343ad1110ac3
Bouzomita Naim <damdoumbouz@gmail.com>
2023-12-13 00:03:41 +0100

    calanderOk

---
+++ Zrayeb-FrontEnd/src/app/components/calenderchldren/calenderchldren.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-calenderchldren',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './calenderchldren.component.html',
+  styleUrl: './calenderchldren.component.css'
+})
+export class CalenderchldrenComponent {
+
+}
