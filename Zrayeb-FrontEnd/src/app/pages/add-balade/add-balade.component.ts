da154a017579be41b0b7edfa5a8f4f79fa3370cd
Bouzomita Naim <damdoumbouz@gmail.com>
2023-12-13 09:59:45 +0100

    all pages added footer and navbar

--- Zrayeb-FrontEnd/src/app/pages/add-balade/add-balade.component.ts
+++ Zrayeb-FrontEnd/src/app/pages/add-balade/add-balade.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { FooterComponent } from "../../components/footer/footer.component";
+import { NavbarComponent } from "../../components/navbar/navbar.component";
 
 @Component({
   selector: 'app-add-balade',
   standalone: true,
-  imports: [],
+  imports: [FooterComponent, NavbarComponent],
   templateUrl: './add-balade.component.html',
   styleUrl: './add-balade.component.css'
 })
