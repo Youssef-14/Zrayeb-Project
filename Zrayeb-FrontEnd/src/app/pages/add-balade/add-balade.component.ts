2d53cdf5fb90d48f46032f23448e5f33803f80c8
Bouzomita Naim <damdoumbouz@gmail.com>
2023-12-13 10:54:39 +0100

    all ok

--- Zrayeb-FrontEnd/src/app/pages/add-balade/add-balade.component.ts
+++ Zrayeb-FrontEnd/src/app/pages/add-balade/add-balade.component.ts
@@ -1,54 +1,12 @@
-import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
-import { faAdd } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-add-balade',
   standalone: true,
-  imports: [CommonModule, FormsModule, FontAwesomeModule, NgbDropdownModule],
+  imports: [],
   templateUrl: './add-balade.component.html',
   styleUrl: './add-balade.component.css'
 })
 export class AddBaladeComponent {
-  faAdd = faAdd;
 
-  months: string[] = [
-    'Janvier',
-    'Février',
-    'Mars',
-    'Avril',
-    'Mai',
-    'Juin',
-    'Juillet',
-    'Août',
-    'Septembre',
-    'Octobre',
-    'Novembre',
-    'Décembre'
-  ];
-
-  years: number[] = [
-    2023, 2024, 2025, 2026, 2027
-  ];
-
-  selectedMonth: string = 'Janvier';
-  selectedYear: number = 2023;
-
-  user: any = {
-    firstName: '',
-    lastName: '',
-    email: '',
-    phone: '',
-    address: ''
-  };
-
-  submitForm() {
-  }
-
-  resetForm() {
-    this.user = {};
-  }
 }
